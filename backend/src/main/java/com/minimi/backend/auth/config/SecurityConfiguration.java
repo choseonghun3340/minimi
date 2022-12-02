d960f8841de2c1d99c19e8986e7698e7a451ce17
seonghun <eptkdltm12@gmail.com>
2022-12-02 20:15:05 +0900

    cors error

--- backend/src/main/java/com/minimi/backend/auth/config/SecurityConfiguration.java
+++ backend/src/main/java/com/minimi/backend/auth/config/SecurityConfiguration.java
@@ -76,6 +76,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
